fa7421d8265a77eed1d38901dcb69c12c14763e0
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-04-27 15:33:29 -0300

    :rocket: Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
-import { ArqueiroMago } from "./modules/arqueir-mago.js"
+import { Mago } from "./modules/mago.js"
+import { Arqueiro } from "./modules/arqueiro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Guerreiro } from "./modules/guerreiro.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
+const guerreiraJorge = new Guerreiro('Jorge', 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroMagoChico]
+//arqueiroBruno.#level = 15
+
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico, guerreiraJorge]
 
 new PersonagemView(personagens).render()
+
+console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio))
+
+console.log(arqueiroBruno)
